c24ce8e229f25b1b418d9b4ed2bdb9b6b226a162
Alejandro Seguí <alesegdia@gmail.com>
2016-03-05 17:29:52 +0100

    testing image loading

--- src/game/gameplayscreen.cpp
+++ src/game/gameplayscreen.cpp
@@ -1,5 +1,6 @@
 #include "gameplayscreen.h"
 #include "game.h"
+#include "assets.h"
 
 #include <allegro5/allegro.h>
 
@@ -26,4 +27,6 @@ void GameplayScreen::render()
 {
 	al_clear_to_color(al_map_rgb(20,20,20));
 	al_set_target_bitmap(al_get_backbuffer(m_game->display()));
+
+	al_draw_bitmap(Assets::instance->characterSheet, 0, 0, 0);
 }
