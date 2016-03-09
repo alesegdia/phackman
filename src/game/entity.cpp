69cbfee32037f92fee79e38e89492ec46311dddd
Alejandro Seguí <alesegdia@gmail.com>
2016-03-09 21:43:17 +0100

    entity update and render methods

--- src/game/entity.cpp
+++ src/game/entity.cpp
@@ -5,3 +5,17 @@ Entity::Entity(float x, float y, std::shared_ptr<Animation> anim)
 {
 
 }
+
+void Entity::update(double delta)
+{
+	m_animData.timer += delta;
+}
+
+void Entity::render()
+{
+	if( m_anim != nullptr )
+	{
+		m_anim->updateData(m_animData);
+		al_draw_bitmap(m_animData.currentFrame, m_x, m_y, 0);
+	}
+}
