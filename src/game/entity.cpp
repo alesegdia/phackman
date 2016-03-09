9758250647257fd4334d655e8f4cdef5274cabe7
Alejandro Seguí <alesegdia@gmail.com>
2016-03-09 02:49:48 +0100

    falling back to class instead of struct for entity

--- src/game/entity.cpp
+++ src/game/entity.cpp
@@ -1,2 +1,7 @@
 #include "entity.h"
 
+Entity::Entity(float x, float y, std::shared_ptr<Animation> anim)
+	: m_x(x), m_y(y), m_anim(anim)
+{
+
+}
--- src/game/entity.h
+++ src/game/entity.h
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <memory>
+
 #include "../core/animation.h"
 #include "assets.h"
 
-struct Entity
+class Entity
 {
-	float x = 0;
-	float y = 0;
-	std::shared_ptr<Animation> anim = nullptr;
-	AnimationData animData;
+public:
+	typedef std::shared_ptr<Entity> SharedPtr;
+
+	Entity( float x, float y, std::shared_ptr<Animation> anim );
+
+	void update(double delta);
+	void render();
+
+private:
+	float m_x = 0;
+	float m_y = 0;
+	std::shared_ptr<Animation> m_anim = nullptr;
+	AnimationData m_animData;
+
 };
