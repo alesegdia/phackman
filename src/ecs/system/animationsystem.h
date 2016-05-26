6b1f47c0c08c26f9a533e6dbbe2cfe288fe878b3
Alejandro Seguí <alesegdia@gmail.com>
2016-05-27 00:53:34 +0200

    reducing animation time

--- src/ecs/system/animationsystem.h
+++ src/ecs/system/animationsystem.h
@@ -19,7 +19,7 @@ public:
 		auto& render_comp = m_world.component<RenderComponent>(e);
 		auto& anim_comp = m_world.component<AnimationComponent>(e);
 
-		anim_comp.animationData.timer += 0.1f;
+		anim_comp.animationData.timer += 0.025f;
 		anim_comp.animation->updateData( anim_comp.animationData );
 		render_comp.bitmap = anim_comp.animationData.currentFrame;
 	}
