c73a5db4b8e014ec491f87720639e55f3a2b53cc
Alejandro Seguí <alesegdia@gmail.com>
2016-09-28 00:28:09 +0200

    moving rendering system def to .cpp

---
+++ src/ecs/system/renderingsystem.cpp
@@ -0,0 +1,29 @@
+
+#include "renderingsystem.h"
+
+RenderingSystem::RenderingSystem(secs::Engine &world)
+	: m_world(world)
+{
+	renderingSystem( true );
+	setNeededComponents<RenderComponent, TransformComponent>();
+}
+
+void RenderingSystem::process(float delta, const secs::Entity &e)
+{
+	auto& render_comp = m_world.component<RenderComponent>(e);
+	auto& transform_comp = m_world.component<TransformComponent>(e);
+
+	assert(render_comp.bitmap != nullptr);
+
+	ALLEGRO_BITMAP* bitmap = render_comp.bitmap;
+
+	float cx = static_cast<float>(al_get_bitmap_width(bitmap)) / 2.f;
+	float cy = static_cast<float>(al_get_bitmap_height(bitmap)) / 2.f;
+
+	al_draw_rotated_bitmap(
+				bitmap, cx, cy,
+				transform_comp.position.x() + cx,
+				transform_comp.position.y() + cy,
+				transform_comp.angle, 0 );
+
+}
--- src/ecs/system/renderingsystem.h
+++ src/ecs/system/renderingsystem.h
@@ -7,32 +7,9 @@ class RenderingSystem : public secs::EntitySystem
 {
 public:
 
-	RenderingSystem( secs::Engine& world )
-		: m_world(world)
-	{
-		renderingSystem( true );
-		setNeededComponents<RenderComponent, TransformComponent>();
-	}
+	RenderingSystem( secs::Engine& world );
 
-	void process(float delta, const secs::Entity &e) override
-	{
-		auto& render_comp = m_world.component<RenderComponent>(e);
-		auto& transform_comp = m_world.component<TransformComponent>(e);
-
-		assert(render_comp.bitmap != nullptr);
-
-		ALLEGRO_BITMAP* bitmap = render_comp.bitmap;
-
-		float cx = static_cast<float>(al_get_bitmap_width(bitmap)) / 2.f;
-		float cy = static_cast<float>(al_get_bitmap_height(bitmap)) / 2.f;
-
-		al_draw_rotated_bitmap(
-					bitmap, cx, cy,
-					transform_comp.position.x() + cx,
-					transform_comp.position.y() + cy,
-					transform_comp.angle, 0 );
-
-	}
+	void process(float delta, const secs::Entity &e) override;
 
 private:
 	secs::Engine& m_world;
