da6393232949900f8b02bc4284602af573bd9fbc
Alejandro Seguí <alesegdia@gmail.com>
2018-05-21 02:06:20 +0200

    adding pi definitions

--- src/ecs/system/facingrenderingsystem.h
+++ src/ecs/system/facingrenderingsystem.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <secs/secs.h>
+#include <cmath>
 #include "../component/components.h"
+
+#define M_PI       3.14159265358979323846   // pi
+#define M_PI_2     1.57079632679489661923   // pi/2
 
 /**
  * @brief The FacingRenderingSystem updates changes TransformComponent angle
